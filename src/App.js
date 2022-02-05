26d2575a5cf318df04726f658d8f2be7c42cda9e
Peerawit Wandech <peerawit.wandech@scb.co.th>
2022-02-05 21:27:45 +0700

    project question2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [categories, setCategories] = useState([])
+  const [filterCategory, setFilterCategory] = useState([])
+
+  useEffect(async () => {
+    const result = await axios('https://api.publicapis.org/categories');
+
+    setCategories(result.data.categories)
+    setFilterCategory(result.data.categories)
+  }, [])
+
+  const searchCategory = (cateName) => {
+    const searchCate = categories.filter(category => category.toLowerCase().match(cateName))
+    setFilterCategory(searchCate)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className={'content'}>
+        <input type={'text'} name={'typingCate'}
+               className={'text-box'}
+               placeholder={'search category..'}
+               onChange={(e) => {
+                 searchCategory(e.target.value)
+               }}/>
+        <TableCategory categories={filterCategory}/>
+      </div>
     </div>
   );
+}
+
+
+const TableCategory = ({categories}) => {
+  return (
+    <table>
+      <tbody>
+      <tr>
+        <th>Category</th>
+      </tr>
+      {categories.map((val, key) => {
+        return (
+          <tr key={key}>
+            <td>{val}</td>
+          </tr>
+        )
+      })}
+      </tbody>
+    </table>
+  )
 }
 
 export default App;
